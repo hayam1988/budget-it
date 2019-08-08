fb6d9934e5be9fb47256606960223811b64f5b1b
Ashley Hay <hayashley@msn.com>
2019-08-07 23:40:29 -0700

    started ui and added moment.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 /* grey  (background-color: #282c34;)  and orange theme color*/
 
-
+import moment from 'moment';
 import React from 'react';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 class App extends React.Component {
 state = {
+startDate: moment(),
 budget: [{
-  text: '',
-  completed: true,
+  amount: '',
+  
 
 }]
 }
 
-
+/*$55 will be budget amount */
   render() {
+    const selectedDate = this.state.startDate.format("ll");
     return (
+      
       <div className="App">
         <header className="App-header">
-    BUDGET-IT 
+            BUDGET-IT &nbsp;|&nbsp;|&nbsp; Daily Balance
         </header>
+        <div className= "time"> Today <div>{selectedDate}</div></div> 
+      
+      <div className= "funds"> $55 /day</div>
+        
+      <TextField
+        id="filled-name"
+        label="Name"
+        className={classes.textField}
+        value={values.name}
+        onChange={handleChange('name')}
+        margin="normal"
+        variant="filled"
+      />
+      
+      
       </div>
+
+   
+
+      
     );
   }
 
@@ -29,11 +54,11 @@ budget: [{
 }
 export default App;
 
-function Item({text, completed, setexpense}) {
-  return (<div className = "item">
+function funds({text, completed, setexpense}) {
+  return (<div className = "funds" >
     
     </div>)
 
-/* this ii this is what be the bughet expenses*/ 
+/* this is what will be the bughet expenses*/ 
 
 }
